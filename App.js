53b03cae2d280924817effb2e7fdce748dbfd88d
Slocx <33150302+Slocx@users.noreply.github.com>
2022-02-03 16:13:07 +0100

    :tada: initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text></Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
